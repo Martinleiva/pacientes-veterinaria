8ac807749a1159650a9c4e2662a56d95c1964d3c
Martin <martin_leiva13@hotmail.com>
2020-01-14 15:42:28 -0300

    Se agrego funciones para trabajar con el local storege

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import './bootstrap.min.css';
 import Header from './components/Header';
 import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
 
 
 class App extends Component {
 
   state = {
     citas: []
+  }
+
+  //Cuando la app carga
+  componentDidMount(){ //Como document.ready de jquery
+    const citasLS = localStorage.getItem('citas');
+
+    if(citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS),
+      });
+    }
+  }
+  
+  //Cuando eliminamos o agregamos una nueva cita
+  componentDidUpdate(){
+    localStorage.setItem('citas', JSON.stringify(this.state.citas));
   }
 
   crearNuevaCita = (datos) => {
@@ -20,6 +37,20 @@ class App extends Component {
     });
   }
 
+  //Elimina las citas del state
+  eliminarCita = (id) => {
+    //Tomar copia del state
+    const citasActuales = [...this.state.citas];
+
+    //Utiliza filter para sacar id del arreglo
+    const citas = citasActuales.filter(cita => cita.id !== id)
+
+    //Actualizar el state
+    this.setState({
+      citas,
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -27,6 +58,12 @@ class App extends Component {
         <div className="row">
           <div className="col-md-10 mx-auto">
             <NuevaCita crearNuevaCita={this.crearNuevaCita} />
+          </div>
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas 
+              citas={this.state.citas} 
+              eliminarCita={this.eliminarCita}  
+            />
           </div>
         </div>
       </div>
